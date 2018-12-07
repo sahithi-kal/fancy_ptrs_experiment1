9e367cdee304af0e9c6d16a005d840592c20416e
Sahithi Kalvakota <sahithi@dhcp-rhodes-2904.redrover.cornell.edu>
2018-12-07 10:58:45 -0500

    added Linked List

--- main.cpp
+++ main.cpp
@@ -17,8 +17,8 @@
      }
  
     uint machine_id;
-    uint slab_id;
-    uint offset;
+    uint slab_id;  //start = beginning of the region we allocate for the
+    uint offset;   //current place in memory => number of nodes*size of one node
  }
  
  there exists table of size machine_id x slab_id at some known position in machine
@@ -30,13 +30,40 @@
  - write LinkedList structure:
  
  struct Linked_List {
-     int val;
-     fancy_ptr next;
+    //include pointer to start of region
+    Linked_List_Node first;
+    val max_size;
  }
+ 
+ struct Linked_List_Node{
+    int val;
+    Linked_List_Node next;
+ }
+ 
+ constructor for Linked List should take in max size of list and pointer to start of region it gets to take memory from
  
  -create two "threads" by calling malloc twice with some large number
  -use "thread_ids" instead of "machine_ids"
  -create 2x1 table at start of "tread"
  -put 3-node linked_list in slab (at fixed offset) and fill in table
- 
 */
+
+//template<class T>
+//class Fancy_Ptr{
+//
+//    uint machine_id;
+//    uint start;
+//    uint offset;
+//    
+//    T operator*() const{
+//        return
+//    }
+//
+//
+//}
+
+
+
+
+
+
